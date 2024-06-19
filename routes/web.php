db7f57317b877f9aa7cb0c500b6eb8b03d9a4b17
Fl1sh <artem.barkovskyi@oa.edu.ua>
2024-06-19 21:35:35 +0300

    Lab13

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::middleware([
 Route::resource('rest', RestTestController::class)->names('restTest');
 
 Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('process-video', [DiggingDeeperController::class,'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [DiggingDeeperController::class,'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
 
